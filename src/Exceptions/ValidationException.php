fc90e8dc3e6b84706fd18c4d814f8db131c7e706
Victor Selukov <v.selukov@wsmgroup.ru>
2021-03-19 01:10:51 +0300

    добавил возможность формировать сообщение исключения на основе violations валидатора

--- src/Exceptions/ValidationException.php
+++ src/Exceptions/ValidationException.php
@@ -3,9 +3,26 @@
 
 namespace Victor78\SmsReg\Exceptions;
 
-
+use Symfony\Component\Validator\ConstraintViolationInterface;
+use Symfony\Component\Validator\ConstraintViolationListInterface;
 
 class ValidationException extends BaseException
 {
+    public static function createFromViolationList(ConstraintViolationListInterface $violationList, ?string $className): self
+    {
+        $text = "Ошибка валидации $className. ";
+        /**
+         * @var ConstraintViolationInterface $violation
+         */
+        foreach ($violationList as $violation)
+        {
+            $messages[] =
+            $text .= 'Параметр ' . $violation->getPropertyPath() . ': "' . $violation->getMessage() . '" ';
+        }
 
+
+
+        $exception = new self($text);
+        return $exception;
+    }
 }
